cf3c50f75391600b578ac05074fdc923c31c4cf7
Neil <mallarineil9@gmail.com>
2024-07-10 03:11:42 +0800

    Define get request for /api/users and add error handler for duplicate username for post request to /api/users

--- controllers/users.js
+++ controllers/users.js
@@ -2,7 +2,13 @@ const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 
-usersRouter.post('/', async (request, response) => {
+usersRouter.get('/', async (request, response) => {
+    const users = await User.find({})
+
+    response.json(users)
+})
+
+usersRouter.post('/', async (request, response, next) => {
     const { username, name, password } = request.body
 
     const saltRounds = 10
@@ -14,9 +20,12 @@ usersRouter.post('/', async (request, response) => {
         passwordHash,
     })
 
-    const savedUser = await user.save()
-
-    response.status(201).json(savedUser)
+    try {
+        const savedUser = await user.save()
+        response.status(201).json(savedUser)
+    } catch (error) {
+        next(error)
+    }
 })
 
 module.exports = usersRouter
