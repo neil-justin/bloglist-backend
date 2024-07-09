cd1dbc93f9c4e88d7a8b241649e0f72daf242006
Neil <mallarineil9@gmail.com>
2024-07-10 02:52:34 +0800

    Define post request for /api/users path

---
+++ models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true // this ensures the uniqueness of username
+    },
+    name: String,
+    passwordHash: String,
+    notes: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Note'
+        }
+    ],
+})
+
+userSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+module.exports = mongoose.model('User', userSchema)
